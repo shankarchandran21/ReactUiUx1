0ca51116f87445f1c1c0e5c1a4f07e9b18954089
Shankar Chandran <shankarchandran21@gmail.com>
2023-03-12 15:41:39 +0530

    Day2 navbar_completed header_In_process

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import "./navbar.css"
 import logo from "../../assets/logo.svg"
-import {RiMenu3Line , RiCloseLine} from "react-icons"
+import {FaBars} from "react-icons/fa"
+import {RxCross1} from "react-icons/rx"
+
+const Menu = ()=>{
+  return<>
+     <p><a href="#home">Home</a></p>
+      <p><a href="#wgpt3">What is GPT?</a></p>
+      <p><a href="#possibility"></a>Open AI</p>
+      <p><a href="#features"></a>Case Studies</p>
+      <p><a href="#blog"></a>Library</p>
+  </>
+}
 const Navbar = () => {
+  const [toggleMenu,setToggleMenu] =React.useState(false);
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-link'>
@@ -10,16 +22,30 @@ const Navbar = () => {
             <img src={logo} alt="logo" />
         </div> 
         <div className='gpt3__navbar-link_container'>
-      <p><a href="#home">Home</a></p>
-      <p><a href="#wgpt3">What is GPT?</a></p>
-      <p><a href="#possibility"></a>Open AI</p>
-      <p><a href="#features"></a>Case Studies</p>
-      <p><a href="#blog"></a>Library</p>
+            <Menu/>
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
         <p>Sign in</p>
         <button type='button'>Sign up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu ?<RxCross1 color="#fff" size={27} onClick={()=>setToggleMenu(false)}/>
+        :<FaBars  color="#fff" size={27} onClick={()=>setToggleMenu(true)}/>  
+      }
+      {
+        toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+              <div className='gpt3__navbar-menu_container-link'>
+                <Menu/>
+                   <div className='gpt3__navbar-menu_container-link-sign'>
+                       <p>Sign in</p>
+                       <button type='button'>Sign up</button>
+                   </div>
+              </div>
+          </div>  
+        )
+      }
       </div>
     </div>
   )
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import "./header.css"
 function Header() {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+        <div className='gpt3__header-content'>
+            <h1></h1>
+        </div>
+    </div>
   )
 }
 
