4997ed195cdad2aa4a6083501cfa8cf9f4d6253f
Shankar Chandran <shankarchandran21@gmail.com>
2023-03-13 22:48:50 +0530

    Day3 header_completed whatgpt3_ recyling_components_in_progressing

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Features from '../features/Features'
 import "./whatGPT3.css"
 function WhatGPT3() {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+        <div className='gpt3__whatgpt3-features'>
+            <Features/>
+        </div>
+        <div className='gpt3__whatgpt3-heading'>
+            <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+            <p>Explore The Library</p>
+        </div>
+        <div className='gpt3__whatgpt3-container'>
+          <Features/>
+          <Features/>
+          <Features/>
+        </div>
+    </div>
   )
 }
 
