5128c2f53c41e1a2f67a2be4c0250ea702ed2177
Shankar Chandran <shankarchandran21@gmail.com>
2023-03-15 23:14:51 +0530

    possibility_blog_completed footer_in_processing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,9 +8,9 @@ const Menu = ()=>{
   return<>
      <p><a href="#home">Home</a></p>
       <p><a href="#wgpt3">What is GPT?</a></p>
-      <p><a href="#possibility"></a>Open AI</p>
-      <p><a href="#features"></a>Case Studies</p>
-      <p><a href="#blog"></a>Library</p>
+      <p><a href="#possibility">Open AI</a></p>
+      <p><a href="#features">Case Studies</a></p>
+      <p><a href="#blog">Library</a></p>
   </>
 }
 const Navbar = () => {
